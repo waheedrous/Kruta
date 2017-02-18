113960d3d9116fc8cc1e9bcb5361d0b512daa27c
Ahmad Hirbawi <ahirbawi@aamlive.com>
2017-02-18 14:42:24 -0800

    Add Telerik Grid

--- Presentation/HPE.Kruta.Web/Controllers/HomeController.cs
+++ Presentation/HPE.Kruta.Web/Controllers/HomeController.cs
@@ -1,13 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using HPE.Kruta.DataAccess;
+using HPE.Kruta.Model;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace HPE.Kruta.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private ModelDBContext db = new ModelDBContext();
+
         [Authorize]
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Queues_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            IQueryable<vDocumentQueue> vdocumentqueues = db.vDocumentQueues;
+            DataSourceResult result = vdocumentqueues.ToDataSourceResult(request);
+
+            return Json(result);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
