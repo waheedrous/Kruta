3de439c28c42daeda23e3b3a48c6a35ecde89be0
Hirbawi <ahmad.hirbawi@hpe.com>
2017-02-24 21:35:45 -0800

    Grid update

--- Presentation/HPE.Kruta.Web/Scripts/DocumentQueue.js
+++ Presentation/HPE.Kruta.Web/Scripts/DocumentQueue.js
@@ -49,11 +49,18 @@ function doAssign() {
         ShowInformationModal('Notification', 'Please select at least one document from the queue.');
     }
     else {
-        AsyncConfirmYesNo(yesFunction, noFunction);
+        // show the Assign Modal
+        var confirm = $("#assignModal");
+        $('#routingControlStaffList').prop('selectedIndex', 0);
+        confirm.modal('show');
     }
 }
 
-function AsyncConfirmYesNo(yesFunction, noFunction) {
+function showConfirmModal() {
+    asyncShowConfirmModal(yesFunction, noFunction);
+}
+
+function asyncShowConfirmModal(yesFunction, noFunction) {
     var confirm = $("#confirmationModal");
     confirm.modal('show');
     confirm.find("#btnYesConfirmYesNo").off('click').click(function () {
@@ -95,77 +102,6 @@ function yesFunction() {
             console.log(xhr.responseText);
         }
     });
-
-    //$.ajax({
-    //    type: 'GET',
-    //    url: base_url + '/AdvisorDashboard/Summary/FilterSections/?assetDescriptionDistinctRowNumbers=' + selectedValues + "&teamMemberKey=" + teamMemberKey,
-    //    data: {},
-    //    success: function (result) {
-    //        if (result) {
-    //            var viewModel = JSON && JSON.parse(result) || $.parseJSON(result);
-    //            // Sales/Assets
-    //            $("#SalesAssetsYearToDateSalesLabel").text(viewModel.SalesAssetsYearToDateSalesLabel);
-    //            $("#SalesAssetsLast12MonthSalesLabel").text(viewModel.SalesAssetsLast12MonthSalesLabel);
-    //            $("#SalesAssetsLastYearSalesLabel").text(viewModel.SalesAssetsLastYearSalesLabel);
-    //            $("#SalesAssetsCurrentAssetsLabel").text(viewModel.SalesAssetsCurrentAssetsLabel);
-    //            // Pipeline Summary
-    //            $("#PipelineSummaryThisMonthLabel").text(viewModel.PipelineSummaryThisMonthLabel);
-    //            $("#PipelineSummaryNextMonthLabel").text(viewModel.PipelineSummaryNextMonthLabel);
-    //            $("#PipelineSummaryNext3MonthsLabel").text(viewModel.PipelineSummaryNext3MonthsLabel);
-    //            $("#PipelineSummaryNext12MonthsLabel").text(viewModel.PipelineSummaryNext12MonthsLabel);
-
-    //            var thisMonthLink = $(".PipelineSummary .title.this-month a");
-    //            var thisMonthLabel = $(".PipelineSummary .title.this-month span");
-    //            var nextMonthLink = $(".PipelineSummary .title.next-month a");
-    //            var nextMonthLabel = $(".PipelineSummary .title.next-month span");
-    //            var next3MonthsLink = $(".PipelineSummary .title.next-three-months a");
-    //            var next3MonthsLabel = $(".PipelineSummary .title.next-three-months span");
-    //            var next12MonthsLink = $(".PipelineSummary .title.next-twelve-months a");
-    //            var next12MonthsLabel = $(".PipelineSummary .title.next-twelve-months span");
-
-    //            if (viewModel.PipelineSummaryThisMonthShowLink) {
-    //                thisMonthLink.show();
-    //                thisMonthLabel.hide();
-    //            }
-    //            else {
-    //                thisMonthLink.hide();
-    //                thisMonthLabel.show();
-    //            }
-    //            if (viewModel.PipelineSummaryNextMonthShowLink) {
-    //                nextMonthLink.show();
-    //                nextMonthLabel.hide();
-    //            }
-    //            else {
-    //                nextMonthLink.hide();
-    //                nextMonthLabel.show();
-    //            }
-    //            if (viewModel.PipelineSummaryNext3MonthsShowLink) {
-    //                next3MonthsLink.show();
-    //                next3MonthsLabel.hide();
-    //            }
-    //            else {
-    //                next3MonthsLink.hide();
-    //                next3MonthsLabel.show();
-    //            }
-    //            if (viewModel.PipelineSummaryNext12MonthsShowLink) {
-    //                next12MonthsLink.show();
-    //                next12MonthsLabel.hide();
-    //            }
-    //            else {
-    //                next12MonthsLink.hide();
-    //                next12MonthsLabel.show();
-    //            }
-
-    //            // Asset Breakdown chart
-    //            LoadAssetBreakdown(selectedValues);
-    //        }
-    //    },
-    //    error: function (xhr, ajaxOptions, thrownError) {
-    //        console.log(xhr.responseText);
-    //    }
-    //});
-
-
 }
 function noFunction() {
     // nothing for now
