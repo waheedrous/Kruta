15f59a41f3afcba02389d316dcb0959c14625191
Hirbawi <ahmad.hirbawi@hpe.com>
2017-03-08 23:59:37 -0800

    after rebase

--- Presentation/HPE.Kruta.Web/Controllers/BaseController.cs
+++ Presentation/HPE.Kruta.Web/Controllers/BaseController.cs
@@ -9,6 +9,10 @@ namespace HPE.Kruta.Web.Controllers
     //[KrutaAuthorize]
     public class BaseController : Controller
     {
+        //public ObjectCache _cache = MemoryCache.Default;
+        public QueueManager _queueManager;
+        public UserManager _userManager;
+
         public int LoggedInUserId
         {
             get
