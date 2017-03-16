e0c69adf885c931c5ac36c8819c7aa76f821350b
Hirbawi <ahmad.hirbawi@hpe.com>
2017-03-15 22:23:04 -0700

    Add prop case info page

---
+++ Presentation/HPE.Kruta.Web/Controllers/PropertyCaseInformationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HPE.Kruta.Web.Controllers
+{
+    public class PropertyCaseInformationController : Controller
+    {
+        // GET: PropertyCaseInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
